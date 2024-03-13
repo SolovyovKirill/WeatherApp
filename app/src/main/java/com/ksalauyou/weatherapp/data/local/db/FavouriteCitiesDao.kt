4a18dcff3ffc671cf6a78aa4d72208043aa1d18c
Salauyou Kiryl <Solowyow.kirya@gmail.com>
2024-03-13 15:15:28 +0300

    add create room db

---
+++ app/src/main/java/com/ksalauyou/weatherapp/data/local/db/FavouriteCitiesDao.kt
@@ -0,0 +1,25 @@
+package com.ksalauyou.weatherapp.data.local.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ksalauyou.weatherapp.data.local.model.CityDbModel
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavouriteCitiesDao {
+
+    @Query("SELECT * FROM favourite_cities")
+    fun getFavouriteCities(): Flow<List<CityDbModel>>
+
+    @Query("SELECT EXISTS (SELECT * FROM favourite_cities WHERE id=:cityId LIMIT 1)")
+    fun observeIsFavourite(cityId: Int): Flow<Boolean>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addFavouriteCity(cityDbModel: CityDbModel)
+
+    @Query("DELETE FROM favourite_cities WHERE id=:cityId")
+    suspend fun removeFavouriteCity(cityId: Int)
+
+}
